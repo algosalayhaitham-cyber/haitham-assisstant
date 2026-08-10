4bf67a09a90b7258e6f771515cf728358f7cb208
algosalayhaitham-cyber <algosalayhaitham@gmail.com>
2026-08-10 23:54:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ if (form && input && messages) {
 
     try {
 
-      const res = await fetch('/chat', {
+      const res = await fetch('/api/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
